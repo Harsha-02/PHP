7cc104d64b7892b9fce53caf861af6001960c578
Harsha K A <97173356+Harsha-02@users.noreply.github.com>
2023-10-25 10:27:43 +0530

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,28 @@
+<?php
+        // put your code here
+        echo "printing...";
+        echo "hello";
+        $name='john';
+        $number=10;
+        $pi=3.14;
+        
+        print "name: ".$name."<br>";
+        echo "Number: ".$number."<br>";
+        echo "pi Value: ".$pi."<br>";
+        $flag=false;
+        $value=NULL;
+        
+        echo gettype($name)."<br>";
+        echo gettype($number)."<br>";
+        echo gettype($pi)."<br>";
+        echo gettype($flag)."<br>";
+        echo gettype($value)>"<br>";
+        
+        $x=5;
+        $y=2;
+        $z= $x ** $y;
+        echo $z;
+        
+        
+        
+        ?>
